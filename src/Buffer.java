2c2ad119e59232462921242ead07844c234f948a
kyungwan2023F <kyungwan@vt.edu>
2024-11-03 14:48:12 -0500

    ..
    .

---
+++ src/Buffer.java
@@ -0,0 +1,21 @@
+public class Buffer {
+    //~ Fields ................................................................
+    private static final int BLOCK_SIZE = 8192; // bytes in a block
+    private static final int RECORD_SIZE = 16; // bytes in a record
+       
+    private byte[] data; 
+    
+    
+    //~ Constructors ..........................................................
+    public Buffer() {
+        
+    }
+    //~Public  Methods ........................................................
+    public byte[] getBlock(int block) {
+        return data;
+    }
+    
+    public int blocksize() {
+        return BLOCK_SIZE;
+    }
+}
